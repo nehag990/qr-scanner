d3208a051eb03fc7cc2106a90569d974b494867f
200020088 <neha@ge.com>
2017-10-10 22:32:52 -0400

    updates functionality

--- app/js/main.js
+++ app/js/main.js
@@ -92,7 +92,11 @@ window.addEventListener("DOMContentLoaded", () => {
   //Hide dialog
   function hideDialog() {
     copiedText = null;
-    window.location = sheetsURL;
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", sheetsURL, true);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+    xhr.send();
+    //window.location = sheetsURL;
     textBoxEle.value = "";
     sheetsURL = "";
     if (window.iOS) {
